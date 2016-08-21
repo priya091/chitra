9a3e17b858993fa3ce5349c6ce883f3c89c9006b
priya091 <rboopathisasikala@gmail.com>
2016-08-21 16:36:13 +0530

    Add files via upload

---
+++ heloo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class hell0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner in = new Scanner(System.in);
+for(int i=1;i<=5;i++)
+{
+	System.out.println("Hello");
+}
+
+	}
+
+}
